e5146e5898e7387696ace4b00f4b0a0eb617e358
Luca Brugaletta <luca.brugaletta@outlook.com>
2024-03-22 18:39:26 +0100

    feature: added rationale of the code and clarity on the printed statements

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import random
 class GameState:
-    """Class representing the state of the game."""
+    """Class representing the state of a Connect Four game.
+    
+    The game is played on a 6x7 board reprensented by a 2D list, initially filled with '-'.
+    Players can place their pieces in any column, and the piece will fall to the lowest possible row.
+    The game is won by placing four pieces in a row, column, or diagonal.
+    When the game starts it will ask for the number of players: 
+    - if the number is 1 the game will be played against the computer.
+        The computer takes his moves randomly - possible improvement would be to implement a simple ai with a minimax algorithm.
+    - if the number is 2 the game will be played by two players, and they will choose the moves alternatively.
+
+    The design of the class is simple, it has a constructor that initializes the game state, a method to get the valid moves,
+    a method to perform a move, a method to check if the current player won the game and a method to check if the board is full,
+    in this case the outcome will be a draw. 
+    The game loop is started by the start_game method, that will print the current board, ask for the next move,
+    if the move is valid it will be performed and board will be updated. At this point, the gameState will check if 
+    the current player won or if the board is full. If one of the endgame conditions is met the game will end printing the result.
+    If the game is not over the current player will be updated and the loop will continue until one of the two endgame conditions is met.
+
+    """
 
     def __init__(self, rows: int, columns: int, num_players: int) -> None:
         """Initialize the game state.
@@ -82,7 +100,6 @@ class GameState:
         for i, row in enumerate(self.board[::-1]):
             print(' '.join(row))
         print(cols_index)
-        print()
 
     def start_game(self):
         """Start the game loop."""
@@ -90,24 +107,20 @@ class GameState:
         self.print_state()
 
         while self.running:
+            print("-------------------")
             valid_moves = self.get_valid_moves()
-
-            if self.current_player == 0:
-                print("Red 'o' player turn  ")
-            else:
-                print("Blue 'x' player turn ")
-
             if self.num_players ==1 and self.current_player == 1:
                 move = random.choice(valid_moves)
             else:
-                move = input("Enter column number (0-6): ")
+                move = input(f"Player '{self.player_simbol[self.current_player]}',  enter your move: ")
                 # To only allow integers, if not a digit the move will be invalidated 
                 if move.isdigit(): 
                     move = int(move)
             
             if move in valid_moves:
                 self.perform_move(move, self.current_player)
-                print(f"current player {self.current_player} move: {move} simbol: {self.player_simbol[self.current_player]}")
+                self.print_state()
+                print(f"Player '{self.player_simbol[self.current_player]}' added a disc in column {move}")
                 if self.is_winner(self.current_player):
                     print(f"Player {self.current_player} wins!")
                     self.running = False
@@ -115,17 +128,18 @@ class GameState:
                     print("It's a draw!")
                     self.running = False
                 self.current_player = 1 - self.current_player
-                self.print_state()
+                
             else:
                 print("Invalid move. Try again.")
 
 if "__main__" == __name__:
     
     print("***Welcome to Connect Four!***")
+
     players_number = input("Enter number of players (1 or 2): ")
     if players_number.isdigit() and int(players_number) in [1, 2]:
         game = GameState(6, 7, int(players_number))
         game.start_game()
     else:
-        print("Invalid number of players. Defaulting to 1 player.")
+        raise ValueError("Invalid number of players. Please enter 1 or 2.")
     
